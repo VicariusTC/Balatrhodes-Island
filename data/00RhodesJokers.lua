209774b55e424c35cd6212909f3db73e7b6835b4
Vicarius <119130564+VicariusTC@users.noreply.github.com>
2025-10-18 11:01:36 +0200

    Projekt Red Bugfix

--- data/00RhodesJokers.lua
+++ data/00RhodesJokers.lua
@@ -471,11 +471,11 @@ SMODS.Joker{
         return {vars = {
             (center.ability.extra.targetHands or G.GAME.round_resets.hands) + center.ability.extra.handDivisor, math.max(center.ability.extra.firstHandDivisorLimit, (center.ability.extra.targetHands or G.GAME.round_resets.hands) - center.ability.extra.handDivisorMinus)}}
     end,
+    add_to_deck = function(self, card, from_debuff)
+        card.ability.extra.targetHands = G.GAME.round_resets.hands
+    end,
     calculate = function(self,card,context)
         if context.hand_drawn then
-            if not card.ability.extra.targetHands then
-                card.ability.extra.targetHands = G.GAME.round_resets.hands
-            end
             if G.GAME.current_round.hands_played + G.GAME.current_round.hands_left > card.ability.extra.targetHands 
             and G.GAME.current_round.hands_played + G.GAME.current_round.hands_left > G.GAME.round_resets.hands + 1 then
                 card.ability.extra.targetHands = G.GAME.current_round.hands_played + G.GAME.current_round.hands_left
